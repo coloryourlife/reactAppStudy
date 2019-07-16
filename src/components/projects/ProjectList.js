da786df0fc1318e97a7c6ad31cea363dcbacf9d4
95hwang <95chris0319@gmail.com>
2019-07-16 19:16:52 +0900

    auth updated

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({projects}) => {
 	return(
 		<div className="project-list section">
 			{ projects && projects.map(project => { 	// && 연산자를 넣은 이유는 projects가 존재하면 오른쪽의 함수를 진행하겠다의 의미
 				return (
-					<ProjectSummary project= {project} key={project.id} />
+					<Link to = { '/project/' + project.id } key={project.id}  >
+						<ProjectSummary project= {project} />
+					</Link>
 				)
 			})} 
 			
